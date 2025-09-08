269b50adc61138a72358e338d60ca0c481b822c0
leeeunjin <enzn@hufs.ac.kr>
2025-09-08 11:08:09 +0900

    REFACTOR: 내부 통신 클라이언트로 설정

--- src/main/java/com/moyeorak/enrollment_service/client/ProgramClient.java
+++ src/main/java/com/moyeorak/enrollment_service/client/ProgramClient.java
@@ -1,9 +1,13 @@
 package com.moyeorak.enrollment_service.client;
 
 import com.moyeorak.enrollment_service.dto.ProgramDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(name = "program", url = "http://localhost:8083")
 public interface ProgramClient {
 
-    // 프로그램 ID로 프로그램 조회
-    ProgramDto getProgramById(Long programId);
+    @GetMapping("/internal/programs/{id}")
+    ProgramDto getProgramById(@PathVariable("id") Long id);
 }
